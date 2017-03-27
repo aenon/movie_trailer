9e40e186381a5bf9f3c661e98c91c247852eb37e
Xilin Sun <s.sn.giraffe@gmail.com>
2017-03-26 22:32:36 -0700

    update comments for movie.py

--- movie.py
+++ movie.py
@@ -4,6 +4,8 @@
 # This file defines the movie class in movie-trailer website.
 
 class movie():
+    # the class for storing information of movies
+    # all values are strings
     def __init__(self, title, youtube_trailer, storyline, poster_image):
         # the title of the movie
         self.title = title
@@ -11,5 +13,5 @@ class movie():
         self.youtube_trailer = youtube_trailer
         # a brief storyline
         self.storyline = storyline
-        # a poster image, preferrably in the original language
+        # url to a poster image, preferrably in the original language
         self.poster_image = poster_image
